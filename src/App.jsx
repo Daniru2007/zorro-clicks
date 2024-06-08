47a71d058132ee1d106cf75080c0a09d91e80ff6
Daniru2007 <daniruayuka@gmail.com>
2024-06-08 06:36:31 +0530

    build updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/utils/Navbar'
 import {
-    HashRouter as Router,
+    BrowserRouter as Router,
     Routes,
     Route,
 } from "react-router-dom";
